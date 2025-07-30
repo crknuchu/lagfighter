4c4ebec3f2034f8673d59be9276315b444dd1f60
Mihailo Bogdanovic <mihailo.bogdanovic1@gmail.com>
2025-07-30 14:50:37 +0200

    fix scene loading

--- menus/button.gd
+++ menus/button.gd
@@ -2,8 +2,8 @@ extends Button
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
+	get_tree().change_scene_to_file("res://level/level.tscn")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("restart"):
-		get_tree().change_scene_to_file("res://level.tscn")
+		get_tree().change_scene_to_file("res://level/level.tscn")
--- menus/button2.gd
+++ menus/button2.gd
@@ -2,4 +2,4 @@ extends Button
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
+	get_tree().change_scene_to_file("res://level/level.tscn")
